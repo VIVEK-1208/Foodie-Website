9e7799835860ea5293baec06376e1da947d3deb5
VIVEK-1208 <vivekbharti1208@gmail.com>
2025-01-17 21:41:18 +0530

    started

---
+++ Front-End/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from './components/NavBar/navbar.jsx'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <NavBar/>
+    </div>
+  )
+}
+
+export default App
