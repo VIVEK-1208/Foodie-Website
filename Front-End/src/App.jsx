1d993c3720b02d850787e9fed6cdc5db9e92afce
VIVEK-1208 <vivekbharti1208@gmail.com>
2025-01-25 16:17:49 +0530

    done

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Navbar from './components/Navbar/navbar.jsx'
+import Navbar from './components/Navbar/navbar'
 import { Route, Routes, useSearchParams } from 'react-router-dom'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx'
 import Cart from './pages/Cart/Cart.jsx'
