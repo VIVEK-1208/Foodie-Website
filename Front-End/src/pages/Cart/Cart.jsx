3d0c7bb2e5b77ed7bd282291c975cdf7593fe2ae
VIVEK-1208 <vivekbharti1208@gmail.com>
2025-01-25 14:26:48 +0530

    Front-End Done

--- Front-End/src/pages/Cart/Cart.jsx
+++ Front-End/src/pages/Cart/Cart.jsx
@@ -51,12 +51,12 @@ const Cart = () => {
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>₹{32}</p>
+              <p>₹{getTotalCartAmount()===0?0:32}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>₹{getTotalCartAmount() + 32}</b>
+              <b>₹{getTotalCartAmount()===0?0:getTotalCartAmount() + 32}</b>
             </div>
           </div>
           <button onClick={()=> navigate('/order')}>PROCEED TO CHECKOUT</button>
